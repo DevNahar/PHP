197816a4edeea73856ade9006cb15607dc722df4
DevNahar <ashekunnahar@gmail.com>
2022-03-24 18:42:08 +0600

    variable

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/* Case Study:
+I have three old hp monitors, not new. I want to sell it on my own website. That’s why I should create a product upload form on my website, then upload my product with specification and available quantity. My monitors have a 15.6 inch display. My monitor’s VGA cable status, yes, I will sell it for 8000 tk. */
+
+$product_name = "Monitor";  //Datatype is string
+$display = 15.6 ;  //Datatype is float/double
+$brand = "hp";  //Datatype is string
+$has_VGA_cable = True;  //Datatype is boolean
+$product_quantity = 3;  //Datatype is integer
+$Condition = "old";  //Datatype is string
+$product_price = 8000;  //Datatype is integer
+
+
+
+?>
