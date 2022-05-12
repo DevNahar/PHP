798e021d9daead807c40721556f079e168ab25f8
DevNahar <ashekunnahar@gmail.com>
2022-05-13 02:36:16 +0600

    assignment 10

--- class#10/index.php
+++ class#10/index.php
@@ -7,7 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
   <title>Registration</title>
 </head>
@@ -153,7 +154,8 @@ Validate - edu email check function -->
             </div>
             <div class=" from-group  mb-3 form-check">
               <input type="checkbox" class="form-check-input" name="check" id="exampleCheck1" required>
-              <label class="form-check-label" for="exampleCheck1">I accept the <a href="#">Terms of service</a> & <a href="#">privacy policy</a> .</label>
+              <label class="form-check-label" for="exampleCheck1">I accept the <a href="#">Terms of service</a> & <a
+                  href="#">privacy policy</a> .</label>
             </div>
             <button type="submit" class="btn btn-primary mt-3 py-2 px-5" name="submit">Submit</button>
           </form>
@@ -175,7 +177,9 @@ Validate - edu email check function -->
 
   </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+  </script>
 
 </body>
 
