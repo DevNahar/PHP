3c7bdefec8ef828df9d8f3463545b65a47a31ed1
DevNahar <ashekunnahar@gmail.com>
2022-05-24 01:06:56 +0600

    Assignment 12

---
+++ class#12/assets/includes/hero.php
@@ -0,0 +1,70 @@
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex align-items-center">
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+      <h1>Better Solutions For Your Business</h1>
+      <h2>We are team of talented designers making websites with Bootstrap</h2>
+      <div class="d-flex justify-content-center justify-content-lg-start">
+        <a href="#contact" class="btn-get-started scrollto">Get Started</a>
+        <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+      </div>
+    </div>
+    <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+      <img src="assets/img/hero-img.png" class="img-fluid animated" alt="">
+    </div>
+  </div>
+</div>
+
+</section><!-- End Hero -->
+
+<!-- ======= Why Us Section ======= -->
+<section id="why-us" class="why-us">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-xl-4 col-lg-5" data-aos="fade-up">
+            <div class="content">
+              <h3>Why Choose RNKASS for your company website?</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Asperiores dolores sed et. Tenetur quia eos. Autem tempore quibusdam vel necessitatibus optio ad corporis.
+              </p>
+              <div class="text-center">
+                <a href="#" class="more-btn">Learn More <i class="bx bx-chevron-right"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-8 col-lg-7 d-flex">
+            <div class="icon-boxes d-flex flex-column justify-content-center">
+              <div class="row">
+                <div class="col-xl-4 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+                  <div class="icon-box mt-4 mt-xl-0">
+                    <i class="bx bx-receipt"></i>
+                    <h4>Corporis voluptates sit</h4>
+                    <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
+                  </div>
+                </div>
+                <div class="col-xl-4 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+                  <div class="icon-box mt-4 mt-xl-0">
+                    <i class="bx bx-cube-alt"></i>
+                    <h4>Ullamco laboris ladore pan</h4>
+                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
+                  </div>
+                </div>
+                <div class="col-xl-4 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
+                  <div class="icon-box mt-4 mt-xl-0">
+                    <i class="bx bx-images"></i>
+                    <h4>Labore consequatur</h4>
+                    <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Why Us Section -->
+
---
+++ class#12/index.php
@@ -0,0 +1,24 @@
+<?php require_once 'assets/includes/header.php' ?>
+
+
+  
+  <main id="main">
+
+
+  <?php require_once 'assets/includes/hero.php' ?>  
+  <?php require_once 'assets/includes/about.php' ?>  
+  <?php require_once 'assets/includes/service.php' ?>  
+  <?php require_once 'assets/includes/testimonial.php' ?>  
+  <?php require_once 'assets/includes/portfolio.php' ?>  
+  <?php require_once 'assets/includes/team.php' ?>  
+  <?php require_once 'assets/includes/contact.php' ?>     
+
+    
+
+  </main><!-- End #main -->
+
+
+  <?php require_once 'assets/includes/footer.php' ?>
+
+
+  
