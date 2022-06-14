994d55d74c82916443e066aec41de1c1e5672420
DevNahar <ashekunnahar@gmail.com>
2022-06-14 19:06:19 +0600

    message

---
+++ class#17_portfolio/admin/includes/navigation.php
@@ -0,0 +1,54 @@
+
+					<!-- Main navigation -->
+					<div class="sidebar-category sidebar-category-visible">
+						<div class="category-content no-padding">
+							<ul class="navigation navigation-main navigation-accordion">
+								
+								<li class="active"><a href="index.php"><i class="icon-home2"></i> <span>Dashboard</span></a>
+								</li>
+								<li ><a href="banner.php"><i class="icon-bookmark"></i> <span>Banners</span></a>
+								</li>
+								<li ><a href="index.html"><i class="icon-clipboard3"></i> <span>services</span></a>
+								</li>
+								<li><a href="index.html"><i class=" icon-stats-decline2"></i> <span>Sections</span></a>
+								</li>
+								<li><a href="index.html"><i class="icon-stack-star"></i> <span>Our Projects</span></a>
+								</li>
+								<li ><a href="index.html"><i class="icon-users2"></i> <span>Our Staff</span></a>
+								</li>
+								<li ><a href="index.html"><i class=" icon-users4"></i> <span>Our clients</span></a>
+								</li>
+								<li ><a href="index.html"><i class=" icon-floppy-disk"></i> <span>Contact Message</span></a>
+								</li>
+
+								<li>
+									<a href="#"><i class="icon-gear"></i> <span>Back Office Setup</span></a>
+									<ul>
+										<li><a href="layout_navbar_fixed.html">Category</a></li>
+										<li><a href="layout_navbar_fixed.html">Designation</a></li>
+										
+									</ul>
+								</li>
+								
+								<li>
+									<a href="#"><i class="icon-stack"></i> <span>Starter kit</span></a>
+									<ul>
+																				
+										<li>
+											<a href="#">3 columns</a>
+											<ul>
+												<li><a href="starters/3_col_dual.html">Dual sidebars</a></li>
+												<li><a href="starters/3_col_double.html">Double sidebars</a></li>
+											</ul>
+										</li>					
+										
+									</ul>
+								</li>
+								
+								<!-- /main -->
+
+								
+							</ul>
+						</div>
+					</div>
+					<!-- /main navigation -->
